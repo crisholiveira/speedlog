bc7cabd09bcb907593acd090eb9ba0575559c7fe
crisholiveira <cris.ho.soares@gmail.com>
2022-01-15 09:39:07 -0300

    tela de login e tela inicial em ejs

--- app.js
+++ app.js
@@ -6,8 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var headerRouter = require('./routes/header');
+var homeRouter = require('./routes/home');
 
 
 var app = express();
@@ -24,8 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/header', headerRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
