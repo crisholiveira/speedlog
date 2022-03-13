3506b4700225d07458af2ee1735143eee282f164
crisholiveira <cris.ho.soares@gmail.com>
2022-03-12 21:19:35 -0300

    Paginacao e consulta view produtos

--- controllers/produtosController.js
+++ controllers/produtosController.js
@@ -1,35 +1,43 @@
-const { Produto, sequelize } = require ('../models')
+const { Produto, sequelize } = require('../models')
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
 
 
 const produtosController = {
     index: async (req, res) => {
-        let produtos = await Produto.findAll()
-        
-            return res.render('produtos', {produtos})            
-        
+        let { page = 1 } = req.query
+        let { count: total, rows: produtos } = await Produto.findAndCountAll({
+            limit: 10,
+            offset: (page - 1) * 10
+
+        })
+        let totalPagina = Math.round(total / 10)
+        return res.render('produtos', { produtos, totalPagina })
+
     },
     findByCod: async (req, res) => {
-        let {id} = req.params;
+        let { id } = req.params;
         let produto = await Produto.findOne({
             where: {
                 id: id
             }
         })
-        return res.render('editarProdutos', {produto})
+        return res.render('editarProdutos', { produto })
     },
     search: async (req, res) => {
-        let {key} = req.query;
+        let { key } = req.query;
         let produtos = await Produto.findAll({
             where: {
-                nome:{
-                    [Op.like]:`%${key}%`   
-                           }
-            }
-            
+                nome: {
+                    [Op.like]: `%${key}%`
+                }
+            },
+            order: [
+                [`nome`, `ASC`]
+            ]
+
         })
-        return res.render('produtos', {produtos})     
+        return res.render('produtos', { produtos })
     }
 }
 module.exports = produtosController
